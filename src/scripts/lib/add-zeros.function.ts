543d8a8c5ad55ea9b027e0c2a6f04657f858128b
Simon Lepel <simbo@simbo.de>
2020-12-12 18:47:57 +0100

    fix add-zeros function

--- src/scripts/lib/add-zeros.function.ts
+++ src/scripts/lib/add-zeros.function.ts
@@ -1,7 +1,5 @@
 export function addZeros(num: number, count = 2): string {
-  let str = `${num}`;
-  for (let i = 1; i < count; i++) {
-    str = parseInt(str, 10) < Math.pow(10, i) ? `${'0'.repeat(i)}${str}` : str;
-  }
-  return str;
+  const str = `${num}`;
+  const i = Math.max(0, count - str.length);
+  return `${'0'.repeat(i)}${str}`;
 }
