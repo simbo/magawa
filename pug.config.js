2792c200c803bb04e33123f1b8f00e99ac8b10a8
Simon Lepel <simbo@simbo.de>
2020-12-16 20:14:52 +0100

    use pixi.js version from package.json for cdn url

--- pug.config.js
+++ pug.config.js
@@ -1,11 +1,17 @@
 const isProd = process.env.NODE_ENV === 'production';
 
+const pixijsVersion = require('./package.json').dependencies['pixi.js'].replace(
+  /[^0-9.]/g,
+  ''
+);
+
 module.exports = {
   doctype: null,
   pretty: false,
   locals: {
     APP_PROD: isProd,
     APP_VERSION: process.env.PACKAGE_VERSION,
-    APP_URI: isProd ? '//simbo.codes/magawa/' : '//localhost:1234/magawa/'
+    APP_URI: isProd ? '//simbo.codes/magawa/' : '//localhost:1234/magawa/',
+    PIXIJS_VERSION: pixijsVersion
   }
 };
