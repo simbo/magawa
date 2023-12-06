4c21f825b16359984bbec0931086f5859af16051
simbo <simbo@simbo.de>
2023-12-06 20:47:09 +0000

    Release v0.10.0 from e1fbdbec4cf777a9c64d888cc51768001f384732

---
+++ assets/icon-name.enum-0ePiKs41.js
@@ -0,0 +1,2 @@
+var r=(t=>(t.Book="book",t.Boom="boom",t.Dead="dead",t.FlagOnGreen="flag-on-green",t.Flag="flag",t.LeftClick="left-click",t.Magawa="magawa",t.Money="money",t.Party="party",t.RightClick="right-click",t.Stopwatch="stopwatch",t.Trophy="trophy",t.Zzz="zzz",t))(r||{});export{r as I};
+//# sourceMappingURL=icon-name.enum-0ePiKs41.js.map
